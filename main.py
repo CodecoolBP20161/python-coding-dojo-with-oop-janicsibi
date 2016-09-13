03ec1ccf32e1742b854e19875dd3322ed7c8b5f6
Komlancz Janos <jkomlancz@gmail.com>
2016-09-13 09:57:18 +0200

    Step_6

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 
 class Contact:
-    pass
+
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contact(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def order(self, string):
+        Supplier.all_orders[self.name] = string
+
+
+class ContactList(Contact):
+    @classmethod
+    def search(cls):
+        pass
+
+
